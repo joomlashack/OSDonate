4e6519e3de3e6f24b46cd76a15c41430098630ab
Bill Tomczak <bill@ostraining.com>
2013-12-17 18:36:18 -0800

    Update language strings reformat to psr2

--- mod_osdonate.php
+++ mod_osdonate.php
@@ -1,13 +1,16 @@
 <?php
-/*
-* @package OSDonate Module 1.0.0 for Joomla 1.6 and 1.5 from OSTraining.com
-* Credit to VeroPlus.com for much of the code and inspiration
-* @copyright Copyright (C) 2010 VeroPlus.com (C) 2011 OsTraining.com. All rights reserved.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see license.txt
-* Joomla! is free software.
-* This extension is available for Joomla! 1.6 and 1.5;
-*/
+/**
+ * @category  Joomla Component
+ * @package   mod_osdonate
+ * @author    VeroPlus.com
+ * @copyright 2010 VeroPlus.com
+ * @copyright 2011, 2013 Open Source Training, LLC. All rights reserved
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @version   1.9.1
+ */
 
 // no direct access
-	defined( '_JEXEC' ) or die( 'Restricted access' );
-	require( JModuleHelper::getLayoutPath( 'mod_osdonate' ) );
+defined('_JEXEC') or die();
+
+require JModuleHelper::getLayoutPath('mod_osdonate');
